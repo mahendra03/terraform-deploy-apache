12b7d8ec13dfdb103db34e326a12840871b1139d
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-28 22:56:58 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
    cidr_block="190.160.0.0/16"
  }
  
- resource "aws_subent" "main" {
+ resource "aws_subnet" "main" {
      vpc_id=aws_vpc.main.id
      cidr_block="190.160.1.0/24"
    }  
