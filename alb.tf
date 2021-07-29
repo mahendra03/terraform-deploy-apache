8d8febdd36a334a935e0c2093d0ff8664d416878
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-29 19:38:54 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -2,18 +2,18 @@ resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = aws_subnet.public.*.id
+  vpc_security_group_ids=["${aws_security_group.webSG.id}"]
+  subnets            = "190.160.1.0/24"
 
   enable_deletion_protection = true
 
   access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
+    bucket  = "mahendrajson"
     prefix  = "test-lb"
     enabled = true
   }
 
   tags = {
-    Environment = "production"
+    Environment = "dev"
   }
 }
