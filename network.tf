5fcfcc82c63895a82777d6c833717edab9fbfb4d
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-29 21:08:32 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,14 +1,13 @@
 resource "aws_vpc" "main" {
-   cidr_block=var.vpc_cidr
+   cidr_block="190.160.0.0/16"
  }
 
-variable "vpc_cidr"{
-   default="190.160.0.0/16"
+variable "sub"{
+   default=["190.160.1.0/20","190.160.2.0/20"]
    }
 
 resource "aws_subnet" "main" {
-  count=2
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet(var.vpc_cidr,8,count.index)
+     vpc_id=aws_vpc.main.id
+     cidr_block=var.sub.id
 }
    
