2fea67998aadd1661b58bd14c638d181b9881305
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-29 20:26:28 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,11 +2,10 @@ resource "aws_vpc" "main" {
    cidr_block="190.160.0.0/16"
  }
  
- resource "aws_subnet" "main" {
-     vpc_id=aws_vpc.main.id
-     cidr_output = [
-          "190.160.1.0/24",
-          "190.16021.0/24",
-            ]
-   }
+resource "aws_subnet" "main" {
+  count             = var.subnet_count
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = data.template_file.public_cidrsubnet[count.index].rendered
+  availability_zone = slice(data.aws_availability_zones.available.names, 0, var.subnet_count)[count.index]
+}
    
