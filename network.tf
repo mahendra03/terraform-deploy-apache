c82d492881df5d82809a067a2e762c9885a09242
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-29 20:01:27 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,9 +1,16 @@
 resource "aws_vpc" "main" {
    cidr_block="190.160.0.0/16"
  }
+
+variable "sub'{
+  default=[
+    a="190.160.1.0/24",
+    b="190.16021.0/24"
+    ]
  
  resource "aws_subnet" "main" {
      vpc_id=aws_vpc.main.id
-     cidr_block="190.160.1.0/24"
+     for_each=var.sub
+     cidr_block=each.value
    }  
    
