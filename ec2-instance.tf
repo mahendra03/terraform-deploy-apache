fdfadf70586ea0cbca736e1d26a770e2559196c3
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-29 08:48:33 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,5 +2,33 @@ resource "aws_instance" "ec2" {
     ami="ami-04db49c0fb2215364"
     instance_type="t2.micro"
     user_data="${file("httpd.sh")}"
+    vpc_security_group_ids=aws_security_group.webSG.id
+}
+
+resource "aws_security_group" "webSG" {
+  name        = "webSG"
+  description = "Allow ssh  inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    
+  }
 }
     
