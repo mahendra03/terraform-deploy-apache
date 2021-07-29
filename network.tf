ebb4be5cdc602547d4764f51c29990e4f2d89cd0
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-29 20:34:15 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,8 +2,15 @@ resource "aws_vpc" "main" {
    cidr_block="190.160.0.0/16"
  }
  
-resource "aws_subnet" "main"{
-   vpc_id=aws_vpc.main.id
-   cidr_block=["190.160.1.0/20","190.160.2.0/20"]
+subnetcidr = {  
+  "190.160.0.0/20" = "ap-south-1a"
+  "190.160.1.0/20" = "ap-south-1b"
+}
+
+resource "aws_subnet" "name" {
+  for_each = var.subnetcidr
+  vpc_id = aws_vpc.name.id
+  availability_zone = each.value
+  cidr_block = each.key
 }
    
