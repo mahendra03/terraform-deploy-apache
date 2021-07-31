0e15d30a1bd2885a47cb6496ddd7e1f472bcb820
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 15:16:42 +0530

    Update launch.tf

--- launch.tf
+++ launch.tf
@@ -1,21 +1,6 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "ami-04db49c0fb2215364"
-    values = ["amzn2-ami-hvm-2.0.20210721.2-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["755555152992"] # Canonical
-}
 
 resource "aws_launch_configuration" "as_conf" {
   name          = "web_config"
-  image_id      = data.aws_ami.ubuntu.id
+  image_id      = "ami-04db49c0fb2215364"
   instance_type = "t2.micro"
 }
