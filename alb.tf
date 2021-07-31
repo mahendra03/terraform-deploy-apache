4d7d7e050c7818d27219b663b0583f4f4c74c172
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 10:09:45 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -10,7 +10,7 @@ resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.webSG.id]
+  security_groups    = ["sg-0a5ccbe0d74bc609c"]
   subnets            = ["subnet-00bce002b4aa75e43", "subnet-01cb9e926e6c799f3"]
   
 
