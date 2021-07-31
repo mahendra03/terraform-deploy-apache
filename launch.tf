663c1971d29b067ee0d7ebedd751d551b9e73745
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 14:45:36 +0530

    Create launch.tf

---
+++ launch.tf
@@ -0,0 +1,16 @@
+resource "aws_launch_configuration" "web" {
+  name_prefix = "web-"
+
+  image_id = "ami-04db49c0fb2215364" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type = "t2.micro"
+  key_name = "Mahendra"
+
+  security_groups = [ aws_security_group.webSG.id ]
+
+  user_data="${file("httpd.sh")}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
