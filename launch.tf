31e06fbb5fc24aceeca9dd9be388421a83ac6b7c
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 16:17:29 +0530

    Update launch.tf

--- launch.tf
+++ launch.tf
@@ -1,16 +1,5 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["755555152992"] # Canonical
-}
-
 resource "aws_launch_configuration" "as_conf" {
   name          = "web_config"
-  image_id      = data.aws_ami.ubuntu.id
+  image_id      = "i-0c107582381e57b4e"
   instance_type = "t2.micro"
 }
