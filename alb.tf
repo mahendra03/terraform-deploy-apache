3efcf9a92c2531ffd723e5b4ae23e5a93472b8ec
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 09:54:08 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "test" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.webSG.id]
-  
+  subnets            = ["subnet-00bce002b4aa75e43", "subnet-01cb9e926e6c799f3"]
   
 
   enable_deletion_protection = true
