a055b40ae6503cada34fc6c401b3b739f659675f
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 15:11:27 +0530

    Update launch.tf

--- launch.tf
+++ launch.tf
@@ -1,17 +1,21 @@
-resource "aws_launch_configuration" "web" {
-  name_prefix = "web-"
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-  image_id = "ami-04db49c0fb2215364" # Amazon Linux 2 AMI (HVM), SSD Volume Type
-  instance_type = "t2.micro"
-  key_name = "Mahendra.pem"
-  
-  security_groups = [ aws_security_group.webSG.id ]
-
-  user_data="${file("httpd.sh")}"
-
-  lifecycle {
-    create_before_destroy = true
+  filter {
+    name   = "ami-04db49c0fb2215364"
+    values = ["amzn2-ami-hvm-2.0.20210721.2-x86_64-gp2"]
   }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["755555152992"] # Canonical
 }
 
+resource "aws_launch_configuration" "as_conf" {
+  name          = "web_config"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+}
