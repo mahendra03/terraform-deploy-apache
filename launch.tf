2a12ebac884085755a3885e896589bbc34555220
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 15:26:30 +0530

    Update launch.tf

--- launch.tf
+++ launch.tf
@@ -1,6 +1,37 @@
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["755555152992"] # Canonical
+}
+
 resource "aws_launch_configuration" "as_conf" {
-  name          = "web_config"
-  image_id      = "ami-04db49c0fb2215364"
+  name_prefix   = "terraform-lc-example-"
+  image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "bar" {
+  name                 = "terraform-asg-example"
+  launch_configuration = aws_launch_configuration.as_conf.name
+  min_size             = 1
+  max_size             = 1
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
