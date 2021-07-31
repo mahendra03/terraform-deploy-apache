d52f34bd9822e1202d2426c84d3e07a4690458cb
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 16:20:38 +0530

    Update launch.tf

--- launch.tf
+++ launch.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_configuration" "as_conf" {
   name          = "web_config"
-  image_id      = "i-0c107582381e57b4e"
+  image_id      = "ami-04db49c0fb2215364"
   instance_type = "t2.micro"
 }
