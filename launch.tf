93f1bc7f1b3929600c44ad0eb5554e2d6f6d9252
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 14:50:53 +0530

    Update launch.tf

--- launch.tf
+++ launch.tf
@@ -3,8 +3,8 @@ resource "aws_launch_configuration" "web" {
 
   image_id = "ami-04db49c0fb2215364" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
-  key_name = "Mahendra"
-
+  key_name = "Mahendra.pem"
+  
   security_groups = [ aws_security_group.webSG.id ]
 
   user_data="${file("httpd.sh")}"
