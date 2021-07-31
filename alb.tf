96f66b11b7009c0f22a8da738b041ff5857ffff9
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 09:50:01 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,1 +1,20 @@
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.webSG.id]
+  
+  
 
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = "mahendrajson"
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
