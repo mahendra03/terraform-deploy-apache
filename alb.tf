1ad7c64926d6a0c6e76ae5174bf51258c5c6ce52
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 09:56:33 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,3 +1,11 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
 resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
