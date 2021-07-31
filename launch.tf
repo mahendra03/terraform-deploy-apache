4d4d9a3957344f4ef3bbf1dd509035977e488759
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 16:01:04 +0530

    Update launch.tf

--- launch.tf
+++ launch.tf
@@ -17,7 +17,6 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_launch_configuration" "as_conf" {
-  name_prefix   = "terraform-lc-example-"
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
