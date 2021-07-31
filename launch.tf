9e85db26bf93272b6de9a55c8a6929f0d84dcd12
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 16:24:13 +0530

    Update launch.tf

--- launch.tf
+++ launch.tf
@@ -3,3 +3,11 @@ resource "aws_launch_configuration" "as_conf" {
   image_id      = "ami-04db49c0fb2215364"
   instance_type = "t2.micro"
 }
+
+resource "aws_autoscaling_group" "bar" {
+  name                 = "terraform-asg-example"
+  launch_configuration = aws_launch_configuration.as_conf.name
+  min_size             = 1
+  max_size             = 1
+
+}
